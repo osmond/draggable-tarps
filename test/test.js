7d4d2c97f24be7829988cde16139a534467c0864
Jon Osmond <jonathan.osmond@gmail.com>
2025-06-12 11:04:48 -0500

    Revert "Add nodemailer server for shirt suggestions"

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ console.log('Basic math test passed');
 const html = fs.readFileSync('index.html', 'utf8');
 assert(
   html.includes('class="suggestion-form"') &&
-    html.includes('action="/suggest"'),
+    html.includes('formspree.io/f/xqabqyaa'),
   'Suggestion form markup missing'
 );
 console.log('Form markup exists');
